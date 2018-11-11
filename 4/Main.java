76778f375d1555119a1d7ab136a4ffccadc8a759
edmanuk <43844468+edmanuk@users.noreply.github.com>
2018-11-11 13:52:11 +0300

    Update 4/Main.java

--- 4/Main.java
+++ 4/Main.java
@@ -2,64 +2,57 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main  extends JFrame {
-    JButton button1 = new JButton("AC Milan");
-    JButton button2 = new JButton("Real Madrid");
-    JLabel label = new JLabel("VS", SwingConstants.CENTER);
-    JLabel labelResult = new JLabel("Result: ", SwingConstants.CENTER);
-    JLabel labelResultShow = new JLabel("0", SwingConstants.CENTER);
-    JLabel labelResultShow2 = new JLabel("0", SwingConstants.CENTER);
-    JLabel labelScorer = new JLabel("Last Scorer: ", SwingConstants.CENTER);
-    JLabel labelScorerShow = new JLabel("", SwingConstants.CENTER);
-    JLabel labelWinner = new JLabel("Winner: ", SwingConstants.CENTER);
-    JLabel labelWinnerShow = new JLabel("", SwingConstants.CENTER);
-    JLabel label2 = new JLabel();
-    int value1 = 0;
-    int value2 = 0;
-    String text;
+    private JButton button1 = new JButton("AC Milan");
+    private JButton button2 = new JButton("Real Madrid");
+    private JLabel labelResultShow = new JLabel("0", SwingConstants.CENTER);
+    private JLabel labelResultShow2 = new JLabel("0", SwingConstants.CENTER);
+    private JLabel labelScorerShow = new JLabel("", SwingConstants.CENTER);
+    private JLabel labelWinnerShow = new JLabel("", SwingConstants.CENTER);
+    private int value1 = 0;
+    private int value2 = 0;
+    private String text;
 
-    public void winner (int a, int b) {
+    private void winner(int a, int b) {
         if (a > b)
             labelWinnerShow.setText(button1.getText());
         else if (a == b)
             labelWinnerShow.setText("Draw");
-        else if (a < b)
-            labelWinnerShow.setText(button2.getText());
+        else labelWinnerShow.setText(button2.getText());
     }
 
-    public Main() {
+    private Main() {
         super("Football Match");
         setDefaultCloseOperation(Main.EXIT_ON_CLOSE);
         setSize(450,300);
         setLayout(new GridLayout(4,3));
-        add(button1, BorderLayout.NORTH); add(label); add(button2, BorderLayout.NORTH);
+        add(button1);
+        JLabel label = new JLabel("VS", SwingConstants.CENTER);
+        add(label); add(button2);
+        JLabel labelResult = new JLabel("Result: ", SwingConstants.CENTER);
         add(labelResult); add(labelResultShow); add(labelResultShow2);
-        add(labelScorer); add(labelScorerShow); add(label2);
+        JLabel labelScorer = new JLabel("Last Scorer: ", SwingConstants.CENTER);
+        add(labelScorer); add(labelScorerShow);
+        JLabel label2 = new JLabel();
+        add(label2);
+        JLabel labelWinner = new JLabel("Winner: ", SwingConstants.CENTER);
         add(labelWinner); add(labelWinnerShow);
 
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                value1+=1;
-                text = String.valueOf(value1);
-                labelResultShow.setText(text);
-                labelScorerShow.setText(button1.getText());
-                winner(value1, value2);
-            }
+        button1.addActionListener(e -> {
+            value1+=1;
+            text = String.valueOf(value1);
+            labelResultShow.setText(text);
+            labelScorerShow.setText(button1.getText());
+            winner(value1, value2);
         });
 
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                value2 += 1;
-                text = String.valueOf(value2);
-                labelResultShow2.setText(text);
-                labelScorerShow.setText(button2.getText());
-                winner(value1, value2);
-            }
+        button2.addActionListener(e -> {
+            value2 += 1;
+            text = String.valueOf(value2);
+            labelResultShow2.setText(text);
+            labelScorerShow.setText(button2.getText());
+            winner(value1, value2);
         });
 
         setVisible(true);
